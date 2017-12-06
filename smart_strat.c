5966e964b6a33408b132fcd9878e81a41ead09fe
Kabir <kabirtomer@gmail.com>
2017-12-06 15:44:30 +0530

    Test 1

---
+++ smart_strat.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+
+long long int main(){
+	long long int T,N,Q,i,prod=1,temp;
+	scanf("%lld",&T);
+	while(T--){
+		prod=1;
+		scanf("%lld %lld",&N,&Q);
+		while(N--){
+            scanf("%lld",&temp);
+            if(temp>0)
+            	prod*=temp;
+		}
+		while(Q--){
+			scanf("%lld",&temp);
+			printf("%lld ",temp/prod);
+		}
+		printf("\n");
+	}
+	return 0;
+}
